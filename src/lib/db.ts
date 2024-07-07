bd47a9f37245aa6f89dbf5e4f5825d68b67fa8b7
WinterSunset95 <wintersunset95@gmail.com>
2024-07-07 11:59:10 +0530

    Fixed the lambda function

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,29 +1,6 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand, PutCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const dbClient = new DynamoDBClient();
+const dbClient = new DynamoDBClient({});
 
 export const docClient = DynamoDBDocumentClient.from(dbClient);
-
-export const createNewUser = async (userId: string, email: string) => {
-	const randomUser = await fetch("https://randomuser.me/api/");
-	const randomUserData = await randomUser.json();
-	const imageUrl = randomUserData.results[0].picture.thumbnail;
-	const command = new PutCommand({
-		TableName: "EimiMediaUsers",
-		Item: {
-			userName: userId,
-			userId: userId,
-			email: email,
-			imageUrl: imageUrl,
-		},
-	})
-	try {
-		const response = await docClient.send(command);
-		console.log(response);
-		return response;
-	} catch (err) {
-		console.error(err);
-		return err;
-	}
-}
